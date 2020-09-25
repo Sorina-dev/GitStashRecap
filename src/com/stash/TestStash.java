1c1beb488d284b171751b21300b195e73d01f944
Sorina-dev <cristiansorina@gmail.com>
2020-09-25 10:43:38 +0300

    first commit

---
+++ src/com/stash/TestStash.java
@@ -0,0 +1,20 @@
+package com.stash;
+
+public class TestStash {
+
+public static void main(String[] args) {
+		
+		System.out.println("Modification of our code");
+		System.out.println("Hello Wolrd");
+
+		System.out.println("git stash pop");
+
+	
+	
+	
+	
+		System.out.println("I live Dallas");
+		
+
+	}
+}
