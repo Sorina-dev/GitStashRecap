941c6b1e4b0ae0ddc3c442e3935bb7561fccb11d
Sorina-dev <cristiansorina@gmail.com>
2020-09-25 11:52:49 +0300

    FeatureA

---
+++ src/com/stash/FeatureASecondFile.java
@@ -0,0 +1,7 @@
+package com.stash;
+
+public class FeatureASecondFile {
+public static void main(String[] args) {
+	System.out.println("FeatureA file-2 completed");
+}
+}
--- src/com/stash/FeatureB.java
+++ src/com/stash/FeatureB.java
@@ -1,5 +1,7 @@
 package com.stash;
 
 public class FeatureB {
-
+public static void main(String[] args) {
+	System.out.println("FeatureB");
+}
 }
